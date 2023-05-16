b4f0ca41ccdeb6fd1d3e4c7264b65e84ce28f71c
hstrejoluna <trejolunatutoriales@gmail.com>
2023-05-15 21:12:17 -0500

    Header and Footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
-import './globals.css'
+import '../styles/globals.scss';
+
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h1>hello there</h1>
+        {children}
+        </body>
     </html>
   )
 }

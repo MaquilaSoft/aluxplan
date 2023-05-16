c25df5ff3381a2b5dac437e573e0029513581464
hstrejoluna <trejolunatutoriales@gmail.com>
2023-05-15 21:21:16 -0500

    feature: Header & Footer base

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer>
+      <span>2023 AluxPlan</span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <span>logo</span>
+      <nav>
+        <ul>
+          <li>Aluxes</li>
+          <li>Experience</li>
+          <li>Practical Info</li>
+          <li>Share</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;

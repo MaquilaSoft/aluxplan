1d2941614379d14f769380cd60c1508addb02b9f
hstrejoluna <trejolunatutoriales@gmail.com>
2023-05-17 21:29:40 -0500

    style: logo container

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,26 @@
-import header from './header.module.scss'
-
+import header from "./header.module.scss";
+import Link from "next/link";
+import Image from "next/image";
 const Header = () => {
   return (
     <header className={header.container}>
-      <span>logo</span>
+      <div className={header.logoContainer}>
+        <Link href="/">
+          <Image
+            src="/images/logo.jpeg"
+            alt="Alux Temple Logo"
+            width={70}
+            height={70}
+            className="h-20 w-auto aspect-square mr-4"
+          />
+        </Link>
+        <div>
+          <h1 className="text-white text-4xl">Alux Plan</h1>
+          <p className="text-white text-xl">
+            Gracias por visitar el templo del Alux
+          </p>
+        </div>
+      </div>
       <nav>
         <ul className={header.list}>
           <li className={header.listItems}>Aluxes</li>
